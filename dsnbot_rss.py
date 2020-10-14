2bd69a7bd15565d5559024aa90801b60968d1f6c
starmaid <nicholas.masso.14@gmail.com>
2020-10-14 16:50:17 -0400

    ao3 updates

--- dsnbot_rss.py
+++ dsnbot_rss.py
@@ -17,7 +17,7 @@ try:
     with open(updates_path, 'r') as fp:
         last_update = json.load(fp)
 except:
-    last_update = {"story": "1", "jon": "None"}
+    last_update = {"story": "1", "jon": "None", "ao3": "none"}
 
 # jon updates
 jon_update = False
@@ -28,9 +28,18 @@ if last_update['jon'] != title:
     jon_update = True
     last_update['jon'] = title
 
+# ao3 updates
+ao3_update = False
+ao3 = feedparser.parse('https://archiveofourown.org/tags/17107986/feed.atom')
+title = ao3['entries'][0]['title']
+link = ao3['entries'][0]['link']
+if last_update['ao3'] != title:
+    ao3_update = True
+    last_update['ao3'] = title
+
 
 # send the messages maybe
-if jon_update:
+if jon_update or ao3_update:
     with open(updates_path, 'w') as fp:
         json.dump(last_update, fp)
 
@@ -39,14 +48,20 @@ if jon_update:
     @client.event
     async def on_ready():
         jon_chan = []
+        ao3_chan = []
         for g in client.guilds:
             for c in g.channels:
                 if c.name == 'announcements':
                     jon_chan.append(c)
+                if c.name == 'clancy':
+                    ao3_chan.append(c)
         if jon_update:
             #send a story update to the story channel
             for c in jon_chan:
                 await c.send('`NEW JON ARTICLE:` ' + link)
+        if ao3_update:
+            for c in ao3_chan:
+                await c.send('`NEW FIC:` ' + link)
         await client.close()
 
     def read_token():
